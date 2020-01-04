c30b5b2d0fc1b6e277aaf019b2a3132e518911f4
cmcdonnell <59296450+Jibbaholic@users.noreply.github.com>
2020-01-04 11:02:38 -0500

    Updated DirtyEpGp.lua
    - reworked print formatting
    - preped for reporting to chat
    - changed /command args

--- DirtyEPGP.lua
+++ DirtyEPGP.lua
@@ -1,10 +1,32 @@
+_dirtyepgp = LibStub("AceAddon-3.0"):NewAddon("_dirtyepgp", "AceTimer-3.0", "AceComm-3.0", "AceSerializer-3.0", "NickTag-1.0")
+_CoreRankText = "Core";
+_CoreRankValue = 5;
+_OfficerAltRankValue = 4;
+
+do
+	local _CreateFrame = CreateFrame --api locals
+	local _UIParent = UIParent --api locals
+	
+	--> Info Window
+		_dirtyepgp.janela_info = _CreateFrame ("Frame", "DetailsPlayerDetailsWindow", _UIParent)
+		_dirtyepgp.PlayerDetailsWindow = _dirtyepgp.janela_info
+		
+	--> Event Frame
+		_dirtyepgp.listener = _CreateFrame ("Frame", nil, _UIParent)
+		_dirtyepgp.listener:RegisterEvent ("ADDON_LOADED")
+		_dirtyepgp.listener:SetFrameStrata ("LOW")
+		_dirtyepgp.listener:SetFrameLevel (9)
+		_dirtyepgp.listener.FrameTime = 0
+end
+
+
 function DirtyEpGp_OnLoad(self)
 	self:RegisterEvent("GUILD_ROSTER_UPDATE");
 end
 
 -- slash commands
 function DirtyEpGp_OnEvent(self, event, ...)
-	SlashCmdList["EPGP"] =  CalculatePriority_SlashCmdHandler;
+	SlashCmdList["EPGP"] = Priority_SlashCmdHandler;
 	SLASH_EPGP1 = "/epgp";
 	SLASH_EPGP2 = "/ep";
 	SLASH_EPGP3 = "/gp";
@@ -29,159 +51,186 @@ function tableSortPrio(a,b)
 	end 
 end
 
--- slash command
-function CalculatePriority_SlashCmdHandler(msg)
 
-	_, _, arg1, arg2 = string.find(msg, "%s?(%w+)%s?(.*)")
 
-	if (arg1 == nil) then 
-		arg1 = ""
-	end
+function Priority_SlashCmdHandler(param)
 
-	if (arg2 == nil) then 
-		arg2 = ""
-	end
+---------------------------------------------
+if (not _dirtyepgp.fontstring_len ) then
+	_dirtyepgp.fontstring_len = _dirtyepgp.listener:CreateFontString (nil, "background", "GameFontNormal")
+end
+local _, fontSize = FCF_GetChatWindowInfo (1)
+if (fontSize < 1) then
+	fontSize = 10
+end
+local fonte, _, flags = _dirtyepgp.fontstring_len:GetFont()
+_dirtyepgp.fontstring_len:SetFont (fonte, fontSize, flags)
+_dirtyepgp.fontstring_len:SetText ("DEFAULT NAME")
+local biggest_len = _dirtyepgp.fontstring_len:GetStringWidth()
+---------------------------------------------
 
-	if (arg1 == "" or arg2 == "") then
-		filter = "all"
-	else 
-		filter = arg1 .. " - " .. arg2
-	end
 
-	spec = "";
-	color = "";
 
-	nGuildMembers = GetNumGuildMembers(true);
-	array = {}
-
-	raid_members = 0;
-
-	-- im bad
+	aPlayers = {};
+	
+	-- change this
+	n = 0;
 	max_namelen = 0;
-	junk = " ............................................";
 
-	for i = 1, nGuildMembers do
+	-- get passed arguments
+	_, _, sArg1, sArg2 = string.find( param, "%s?(%w+)%s?(.*)" )
 
-		-- get guild info
-		g_name, g_rank, g_rankIndex, g_level, g_class, g_zone, g_note, g_officernote = GetGuildRosterInfo(i);
+	sArg2 = string.lower(sArg2);
+	sArg1 = string.lower(sArg1);
 
-		-- clean name, check if their in raid with me
-		tname = strsub( g_name, 1, strlen(g_name)-7);
-		ingroup = UnitInRaid(tname);
+	-- load all guild members
+	iGuildMembers = GetNumGuildMembers();
 
-		-- spit officer note to ep / gp, calculate priority
-		ep, gp = strsplit(",", g_officernote);
-		priority = round( ((tonumber(ep) or 0) / (tonumber(gp) or 0)),2 );
+	for i = 1, iGuildMembers do
 
-		-- do some shifty shit to check role
-		if (g_class == "Warrior") then
-			spec = "Melee";
-			color = "|cffc79c6e";
-		elseif (g_class == "Warlock") then
-			spec = "Caster";
-			color = "|cff9482c9";
-		elseif (g_class == "Mage") then
-			spec = "Caster";
-			color = "|cff69ccf0";
-		elseif (g_class == "Druid") then
-			spec = "Misc";
-			color = "|cffff7d0a";
-		elseif (g_class == "Priest") then
-			spec = "Caster";
-			color = "|cffffffff";
-		elseif (g_class == "Rogue") then
-			spec = "Melee";
-			color = "|cfffff569";
-		elseif (g_class == "Shaman") then
-			spec = "Caster";
-			color = "|cff0070de";
-		elseif (g_class == "Hunter") then
-			spec = "Ranged";
-			color = "|cffabd473";
+		insert = 0;
+		sName, sRank, iRankIndex, iLevel, sClass, _, _, sEpGp = GetGuildRosterInfo(i);
+		sCleanName = strsub( sName, 1, string.find(sName, "-")-1 );
+
+		iEp, iGp = strsplit( ",", sEpGp );
+		if (iEp == nil or iGp == nil ) then
+			iEp = 0;
+			iGp = 0;
+			dPriority = 0;
 		else 
-			spec = "Unknown";
-			color = "";
+			dPriority = round( (( tonumber(iEp) or 0 ) / ( tonumber(iGp) or 0 )),2 )
 		end 
 
-		if (g_rank == "Officer" or g_rank == "Raid Leader" or g_rank == "PvP Leader" or g_rank == "Guild Master" ) then
-			g_rank = "Core"
-		end
+		_, _, _, sColor = GetClassColor( string.upper(sClass) );
+		if ( sCleanName == UnitName("player") ) then sColor = "ffffff00" end
 
-		-- check if in group
+
+		bIsInGroup = UnitInRaid( sCleanName );
+
+		if ( bIsInGroup ~= nil ) then 
+			bIsInGroup = 1;
+		else
+			bIsInGroup = 0;
+		end 
+
+		-- get officers+ in core rank
+		if ( iRankIndex <= _CoreRankValue and iRankIndex ~= _OfficerAltRankValue ) then 
+			sRank = _CoreRankText
+			iRankIndex = _CoreRankValue
+		end 
 		
-		-- raid filter
-		--if (string.lower(arg1) ~= "player" and ingroup ~= nil) then
-		-- if filter by spec
-		if (string.lower(arg1) == "spec" and ingroup ~= nil and string.lower(arg2) == string.lower(spec)) then
-			raid_members = raid_members + 1;
-			array[raid_members] = { ["name"] = tname, ["rank"] = g_rank, ["class"] = g_class, ["ep"] = ep, ["gp"] = gp, ["prio"] = priority, ["spec"] = spec, ["color"] = color }	
+		-- get specs
+		if ( sClass == "Warrior" or sClass == "Rogue") then 
+			sSpec = "Melee"
+		elseif ( sClass == "Mage" or sClass == "Warlock" or sClass == "Priest" or sClass == "Druid" or sClass == "Shaman" ) then
+			sSpec = "Caster"
+		elseif ( sClass == "Hunter" ) then 
+			sSpec = "Ranged"
+		end 
 
-		-- if filter by class
-		elseif (string.lower(arg1) == "class" and ingroup ~= nil and string.lower(arg2) == string.lower(g_class)) then
-			raid_members = raid_members + 1;
-			array[raid_members] = { ["name"] = tname, ["rank"] = g_rank, ["class"] = g_class, ["ep"] = ep, ["gp"] = gp, ["prio"] = priority, ["spec"] = spec, ["color"] = color }	
-
-		-- else entire raid
-		elseif(arg1 == "" and ingroup ~= nil ) then
-			raid_members = raid_members + 1;
-			array[raid_members] = { ["name"] = tname, ["rank"] = g_rank, ["class"] = g_class, ["ep"] = ep, ["gp"] = gp, ["prio"] = priority, ["spec"] = spec, ["color"] = color }	
-				--end
-
-		-- 1 player
-		elseif (string.lower(arg1) == "player" and string.lower(arg2) == string.lower(tname)) then
-			raid_members = raid_members + 1;
-			array[raid_members] = { ["name"] = tname, ["rank"] = g_rank, ["class"] = g_class, ["ep"] = ep, ["gp"] = gp, ["prio"] = priority, ["spec"] = spec, ["color"] = color }	
-
-		-- show core class
-		elseif (string.lower(arg1) == "core" and g_rank == "Core" and string.lower(arg2) == string.lower(g_class)) then
-			raid_members = raid_members + 1;
-			array[raid_members] = { ["name"] = tname, ["rank"] = g_rank, ["class"] = g_class, ["ep"] = ep, ["gp"] = gp, ["prio"] = priority, ["spec"] = spec, ["color"] = color }	
-
-		-- show core spec
-		elseif (string.lower(arg1) == "core" and g_rank == "Core" and string.lower(arg2) == string.lower(spec)) then
-			raid_members = raid_members + 1;
-			array[raid_members] = { ["name"] = tname, ["rank"] = g_rank, ["class"] = g_class, ["ep"] = ep, ["gp"] = gp, ["prio"] = priority, ["spec"] = spec, ["color"] = color }	
-
-		end
-		
-		if( raid_members ~= 0 ) then
-			local _, name_len = string.gsub(array[raid_members].name, "[^\128-\193]", "")
-			if (name_len > max_namelen) then
-				max_namelen = name_len;
+		-- fix later
+		-- if /ep core {spec|class}
+		if ( sArg1 == string.lower(_CoreRankText) and string.lower(sRank) == string.lower(_CoreRankText) ) then
+			if ( sArg2 == string.lower(sClass) or sArg2 == string.lower(sSpec) ) then
+				insert = 1;
 			end 
+
+		-- if /ep raid {spec|class}
+		elseif (sArg1 == "raid" and bIsInGroup == 1) then
+			if ( sArg2 == string.lower(sClass) or sArg2 == string.lower(sSpec) ) then
+				insert = 1;
+			end 
+
+		-- if /ep guild {spec|class}
+		elseif ( sArg1 == "guild" ) then
+			if ( sArg2 == string.lower(sClass) or sArg2 == string.lower(sSpec) ) then
+				insert = 1;
+			end 
+
+		-- if /ep player {player_name}
+		elseif ( sArg1 == "player" ) then
+			if ( sArg2 == string.lower(sCleanName) ) then
+				insert = 1;
+			end
+
+		-- if /ep show entire raid
+		elseif ( sArg1 == "" and bIsInGroup == 1 ) then
+			insert = 1;
 		end
-	end
 
-	table.sort(array, tableSortPrio);
-	print("");
-	print("|cff00ff00<Dirty> EPGP: Priority Rank for: " .. filter);
-	for i, members in ipairs(array) do
+		-- if the command was valid
+		if ( insert == 1 ) then 
+			
+			-- populate the array
+			n = n + 1;
+			aPlayers[n] = 
+			{
+				name = sCleanName,
+				rank = sRank,
+				class = sClass,
+				spec = sSpec,
+				color = sColor,
+				ep = iEp,
+				gp = iGp,
+				prio = dPriority,
+				ingroup = bIsInGroup
+			}
 
-		-- clean up
-		local _, name_len = string.gsub(array[i].name, "[^\128-\193]", "")
-
-		-- format the strings
-		-- if name has special char
-		if (name_len ~= strlen(array[i].name)) then 
-			_name = strsub( array[i].name .. junk, 1, ( max_namelen ) + 5 );
-		else 
-			_name = strsub( array[i].name .. junk, 1, ( max_namelen ) + 4 );
 		end
-		_ep = string.format("%03d", array[i].ep);
-		_gp = string.format("%02d", array[i].gp);
-		_prio = string.format("%.2f", array[i].prio);
 		
-		color = array[i].color;
-		-- highlight you
-		if (array[i].name == UnitName("player")) then
-			color = "|cffffff00"
+		_dirtyepgp.fontstring_len:SetText ( sCleanName )
+		local len = _dirtyepgp.fontstring_len:GetStringWidth()
+		if (len > biggest_len) then
+			biggest_len = len
 		end
 
-		-- print cause lazy
-		print(color .. string.format("%02d", tostring(i)) .. ". " .. _name .. " " .. _ep .. "/" .. _gp .. " (" .. _prio .. ")" );
 	end
+
+
+	if (biggest_len > 130) then
+		biggest_len = 130
+	end
+
+
+	-- sort it
+	table.sort(aPlayers, tableSortPrio);
+
+ 	print("");
+	print("|cff00ff00<Dirty> EPGP: Priority Rank for: " .. sArg1 .. " - " .. sArg2);
+	 
+	for i, player in ipairs(aPlayers) do
+		
+		
+		v1 = player.name .. " "
+			_dirtyepgp.fontstring_len:SetText (v1)
+			local len = _dirtyepgp.fontstring_len:GetStringWidth()
+			
+			while (len < biggest_len) do 
+				v1 = v1 .. "."
+				_dirtyepgp.fontstring_len:SetText (v1)
+				len = _dirtyepgp.fontstring_len:GetStringWidth()
+			end			
+
+
+		--local _, name_len = string.gsub(player.name, "[^\128-\193]", "")
+		--z = 14;
+		--if ( name_len < 7 ) then z = 15 end;
+		--spacer = strsub ( "..........................................", 1, (z - name_len ));
+		
+		dPointsBehind = aPlayers[1].prio - player.prio;
+
+
+		--sOutput = --[["|c".. player.color .. --]] string.format("%02d", tostring(i)) .. ". ";
+		sOutput = "|c".. player.color .. string.format("%02d", tostring(i)) .. ". ";
+		sOutput = sOutput .. v1 .. " " --player.name .. " " .. spacer .. " " 
+		sOutput = sOutput .. string.format("%03d", player.ep );
+		sOutput = sOutput .. "/" .. string.format("%02d", player.gp );
+		sOutput = sOutput .. " (" .. string.format("%.2f", player.prio ) .. ")";
+		--sOutput = sOutput .. ", -" .. tostring( string.format("%.2f", dPointsBehind) ) .. ")"
+
+		print( sOutput );
+		--SendChatMessage( sOutput, "PARTY" );
+	end
+
 	print("");
-
-
-end
-
+end 
